609225fbb7205185e1c5394b7343e66fa081638b
DYDYD <njlxyaoxinwei@gmail.com>
2013-10-23 11:45:00 -0400

    Finished Tutorial Part19. Made the Record Page

--- SoundBoard/RecordAudio.xaml.cs
+++ SoundBoard/RecordAudio.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using SoundBoard.Resources;
 
 namespace SoundBoard
 {
@@ -15,6 +16,25 @@ namespace SoundBoard
         public RecordAudio()
         {
             InitializeComponent();
+            BuildLocalizedApplicationBar();
         }
+
+        private void BuildLocalizedApplicationBar()
+        {
+           ApplicationBar = new ApplicationBar();
+           ApplicationBarIconButton recordAudioAppBar = new ApplicationBarIconButton();
+           recordAudioAppBar.IconUri = new Uri("/Assets/AppBar/save.png", UriKind.Relative);
+           recordAudioAppBar.Text = AppResources.AppBarSave;
+           recordAudioAppBar.Click += recordAudioAppBar_Click;
+           ApplicationBar.Buttons.Add(recordAudioAppBar);
+        //   ApplicationBar.IsVisible = false;
+
+        }
+
+        void recordAudioAppBar_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
